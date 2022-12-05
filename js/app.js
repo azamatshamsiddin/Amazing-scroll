318c40097c057a9bf5ef7ad3429198c939c8d0b0
azamatshamsiddin <muratovichazamat@yandex.com>
2022-12-05 17:57:27 +0500

    This page created with Swiper.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+document.querySelectorAll(".slider").forEach((item, index) => {
+	window[`slider${index+1}`] = new Swiper(item, {
+		freeMode: true,
+		centeredSlides: true,
+		mousewheel: true,
+		slidesPerView: 1.75,
+		direction: "vertical",
+    slidesOffsetBefore: -125
+	});
+});
+
+bindSwipers(slider1, slider2, slider3, slider4)
